25c0cf1988e1543c767ca77c0d0ce1d9cf29c492
byteworksng <chibuzorogbu@gmail.com>
2014-08-26 22:12:32 +0100

    Skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+    
+    
+except ImportError
+    from distutils.core import setup
+
+config = [
+            'description':'Transport and Cargo logistics Management Suite', 
+            'author': 'Chibuzor Ogbu',
+            'url': 'http://www.byteworksng.com',
+            'download_url': 'http://www.byteworksng.com/downloads/tackle',
+            'author_email': 'chibuzorogbu@gmail.com',
+            'version': '0.Alpha.1',
+            'install_requires': ['nose'],
+            'packages': ['tackle'],
+            'scripts': [],
+            'name': 'Tackle'
+         ]   
+
+setup(**config)
+        
+    
