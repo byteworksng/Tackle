79d1f5187adf5b0af0ec3ed0dd787d66e9539dcd
byteworksng <chibuzorogbu@gmail.com>
2014-08-26 22:34:59 +0100

    module, entry point

--- tackle/tackle.py
+++ tackle/tackle.py
@@ -1,26 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import *
-from kivy.vector import Vector
 
 
-
-class PongGame(Widget):
+class Ticket(Widget):
     pass
 
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
 
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+class Manifest(Widget):
+    pass
 
 
 class Tackle(App):
     def build(self):
-        return PongGame()
+        pass
 
 if __name__ == '__main__':
     Tackle().run()
